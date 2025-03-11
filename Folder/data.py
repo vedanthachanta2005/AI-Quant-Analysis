3bed5fc64ced80995116cb175f0f7c87c156ce9e
vedanthachanta2005 <vedantha2005@gmail.com>
2025-03-10 22:48:10 -0400

    added data.py file which reads snp500 data

---
+++ Folder/data.py
@@ -0,0 +1,12 @@
+from statsmodels.regression.rolling import RollingOLS
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+import pandas_ta
+import warnings
+warnings.filterwarnings('ignore')
+pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
