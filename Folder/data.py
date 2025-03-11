ca06b4c417dfa3c5f44eb66f1c53d66720c65fc8
vedanthachanta2005 <vedantha2005@gmail.com>
2025-03-11 00:22:28 -0400

    Final update 3/10/2025

--- Folder/data.py
+++ Folder/data.py
@@ -6,7 +6,14 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import yfinance as yf
-import pandas_ta
+#import pandas_ta as ta
 import warnings
 warnings.filterwarnings('ignore')
-pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+snp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+snp500['Symbol'] = snp500['Symbol'].str.replace('.', '-')
+symbols_list = snp500['Symbol'].unique().tolist()
+
+end_date = '2025-03-10'
+start_date = pd.to_datetime(end_date)-pd.DateOffset(50)
+df = yf.download(tickers=symbols_list, start=start_date, end=end_date)
+
