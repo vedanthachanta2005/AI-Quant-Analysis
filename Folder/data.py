1ea773a5677fe43d46525bcf97417cbcb365ba35
vedanthachanta2005 <vedantha2005@gmail.com>
2025-06-15 01:18:47 -0400

    restructed data

--- Folder/data.py
+++ Folder/data.py
@@ -1,22 +1,7 @@
-from statsmodels.regression.rolling import RollingOLS
-import pandas_datareader.data as web
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
-import pandas as pd
-import numpy as np
-import datetime as dt
 import yfinance as yf
-#import pandas_ta as ta
-import warnings
-warnings.filterwarnings('ignore')
-snp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-snp500['Symbol'] = snp500['Symbol'].str.replace('.', '-')
-symbols_list = snp500['Symbol'].unique().tolist()
-
-end_date = '2025-03-10'
-start_date = pd.to_datetime(end_date)-pd.DateOffset(50)
-df = yf.download(tickers=symbols_list,
-                start=start_date, 
-                end=end_date)
-df.stack()
-
+import pandas as pd
+from datetime import date
+#y finance only allows 60 days of data when trying to get low interval data
+data = yf.download("EURUSD=X", start="2025-5-1", end=date.today(), interval='15m')
+data.iloc[-1:,:]
+data.Open.iloc
