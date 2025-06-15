776eb95dfaad1496cfaf5e44f005f4af40caaf16
vedanthachanta2005 <vedantha2005@gmail.com>
2025-06-15 01:42:00 -0400

    made signal function that determines what to do in differnet markets

--- Folder/data.py
+++ Folder/data.py
@@ -5,3 +5,26 @@ from datetime import date
 data = yf.download("EURUSD=X", start="2025-5-1", end=date.today(), interval='15m')
 data.iloc[-1:,:]
 data.Open.iloc
+
+def signal(df): 
+    open = df.Open.iloc[-1]
+    close = df.Close.iloc[-1]
+    previous_open = df.Open.iloc[-2]
+    previous_close = df.Open.iloc[-2]
+
+    #Bearish Market
+    if(open > close and previous_open < previous_close and close<previous_open and open>=previous_close): 
+        return 1
+    #Bullish market
+    elif (open < close and previous_open>previous_close and close>previous_open and open>=previous_close):
+        return 2
+    #return 0 if no clear pattern is there
+    else: 
+        return 0
+    
+signal = []
+signal.append(0)
+for i in range(1, len(data)):
+    df = data[i-1: i+1]
+    signal.append(signal(df))
+data["signal"] = signal
